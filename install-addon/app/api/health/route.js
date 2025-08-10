084e7c40404df13a073c4a3c4125ef9f3e6e12f2
tokiyoriito-arch <tokiyoriito@gmail.com>
2025-08-10 20:26:56 +0900

    Delete install-addon directory

--- install-addon/app/api/health/route.js
+++
@@ -1,7 +0,0 @@
-
-export const runtime = 'nodejs';
-export async function GET(){
-  return new Response(JSON.stringify({ ok:true, time: new Date().toISOString() }), {
-    headers: { 'content-type': 'application/json' }
-  });
-}
